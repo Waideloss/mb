5cd33f95dda4559b405f3bdce3b0b23b67e6623d
Waideloss <psykhokidzxc@gmail.com>
2024-08-02 11:51:00 +0700

    idk

---
+++ lmao.h
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+namespace out()
+{
+    void strout(string string)
+    {
+        cout  << string << endl;
+    }
+}
+namespace math()
+{
+    template <typename T>
+    T sum(T value1, T value2)
+    {
+        value1 + value2
+    }
+}
